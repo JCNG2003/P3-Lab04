81c028473303f418f076ad516203d5ce1dc9a112
JCNG2003 <123774985+JCNG2003@users.noreply.github.com>
2023-04-14 21:55:37 -0500

    Create Turno.java

---
+++ Turno.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package turnosentremedica01;
+
+/**
+ *
+ * @author CESAR
+ */
+public class Turno {
+
+    private String nombre;
+
+    public Turno(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
